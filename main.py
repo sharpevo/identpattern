06a82bd3150ae072b1c7d3f28a5e610980d056a3
sharpevo <sharpevo@gmail.com>
2013-03-29 20:52:17 +0800

    main.py: simple animation

--- main.py
+++ main.py
@@ -10,19 +10,26 @@ from lib.commandstack.commandstack import CommandStack
 
 MAX_HISTORY = 10
 class MainWindow(QMainWindow, Ui_MainWindow):
+    anim = pyqtSignal()
 
     def __init__(self, parent = None):
 
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
+        self.scene_icon = QGraphicsScene()
         self.probar.hide()
-
+        #self.gv_canvas.setBackgroundBrush(QBrush(QColor("#fdf6e3"), Qt.SolidPattern))
+        gradient = QRadialGradient(0,0, 600, 0,0)
+        gradient.setColorAt(0, QColor("#00aaaa"))
+        gradient.setColorAt(1, QColor("#eeeeee"))
+        self.gv_canvas.setBackgroundBrush(QBrush(gradient))
         self.history = CommandStack(MAX_HISTORY)
         self.icon_path = os.path.join(tempfile.gettempdir(), "icon.png")
         self.canvas_path = os.path.join(tempfile.gettempdir(), "canvas.png")
         self.tb_collection.setColumnCount(1)
         self.load_collection(init=True)
         self.generate_icon()
+
 
     def generate_icon(self, code=0, hist=False, ui=True):
         self.code = identicon.generate_icon(code)
@@ -37,13 +44,43 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def update_ui(self):
         self.make_label()
-        scene_icon = QGraphicsScene()
-        scene_icon.addPixmap(QPixmap(self.icon_path))
-        self.gv_icon.setScene(scene_icon)
-        scene_canvas = QGraphicsScene()
-        scene_canvas.addPixmap(QPixmap(self.canvas_path))
-        self.gv_canvas.setScene(scene_canvas)
+        self.pix = QPixmap(self.icon_path)
+        self.scene_icon.clear()
+        self.scene_icon.addPixmap(self.pix)
+        self.gv_icon.setScene(self.scene_icon)
         self.statusbar.clearMessage()
+        self.animation()
+
+    def animation(self):
+        """
+        OutElastic: Quick tiled;
+        InOutElastic: centered then tiled;"""
+
+        mode = 10
+        count = mode * mode
+
+        scene = QGraphicsScene(-350, -350, 700, 700)
+        items = []
+        for i in range(count):
+            item = Pixmap(self.pix)
+            item.pixmap_item.setOffset(-self.pix.width() / 2, -self.pix.height() / 2)
+            item.pixmap_item.setZValue(i)
+            items.append(item)
+            scene.addItem(item.pixmap_item)
+
+        self.gv_canvas.setScene(scene)
+
+        self.group = QParallelAnimationGroup()
+        for i, item in enumerate(items):
+            anim = QPropertyAnimation(item, "pos")
+            anim.setStartValue(QPointF(-self.pix.width()/2, -self.pix.height()/2))
+            anim.setEndValue(QPointF(((i%mode)-mode/2)*self.pix.width() + self.pix.width()/2,
+                    ((i//mode)-mode/2)*self.pix.height() + self.pix.height()/2))
+            anim.setDuration(750+i*25)
+            #anim.setDuration(1500)
+            anim.setEasingCurve(QEasingCurve.InOutBack)
+            self.group.addAnimation(anim)
+        self.group.start()
 
     def make_label(self):
         label_list = []
@@ -89,8 +126,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 
     def load_collection(self,init=False):
 
-        if init:
-            self.show()
+        #if init:
+            #self.show()
         self.thread = Load_Collection(self)
         self.thread.partDone.connect(self.update_probar)
         self.thread.procDone.connect(self.finish_probar)
@@ -127,6 +164,8 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             self.export_file("eps")
         if key == Qt.Key_S:
             self.export_file("jpg")
+        if key == Qt.Key_R:
+            self.animation()
 
         if key == Qt.Key_Delete:
             if self.tb_collection.currentItem():
@@ -196,12 +235,41 @@ class Load_Collection(QThread):
         self.procDone.emit(True)
         self.main_window.setEnabled(True)
 
+class Pixmap(QObject):
+
+    def __init__(self, pix):
+        super(Pixmap, self).__init__()
+
+        self.pixmap_item = QGraphicsPixmapItem(pix)
+        self.pixmap_item.setCacheMode(QGraphicsItem.DeviceCoordinateCache)
+
+    def _set_pos(self, pos):
+        self.pixmap_item.setPos(pos)
+
+    pos = pyqtProperty(QPointF, fset=_set_pos)
+
+class Play_Anim(QThread):
+
+    def __init__(self,main_window, parent=None):
+        super(Play_Anim, self).__init__(parent)
+        self.stopped = False
+        self.completed = False
+        self.main_window = main_window
+
+    def stop(self):
+        self.stopped = False
+
+    def run(self):
+        self.process()
+        self.stop()
+
+    def process(self):
+        self.main_window.anim.emit()
 
 if __name__ == "__main__":
 
     import sys
     app = QApplication(sys.argv)
     main_window = MainWindow()
-    main_window.show()
+    main_window.showFullScreen()
     sys.exit(app.exec_())
-
