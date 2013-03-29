9bc678e78ada5799b85d07a836fa76bf29245adc
sharpevo <sharpevo@gmail.com>
2013-03-29 22:05:38 +0800

    main.py: fullscreen display canvas

--- main.py
+++ main.py
@@ -4,30 +4,28 @@ import os
 import tempfile
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
-from ui_main import Ui_MainWindow
+#from ui_main import Ui_MainWindow
 from lib.identicon import identicon
 from lib.commandstack.commandstack import CommandStack
 
 MAX_HISTORY = 10
-class MainWindow(QMainWindow, Ui_MainWindow):
-    anim = pyqtSignal()
+class MainWindow(QGraphicsView):#QMainWindow, Ui_MainWindow):
 
     def __init__(self, parent = None):
 
         QMainWindow.__init__(self, parent)
-        self.setupUi(self)
-        self.scene_icon = QGraphicsScene()
-        self.probar.hide()
-        #self.gv_canvas.setBackgroundBrush(QBrush(QColor("#fdf6e3"), Qt.SolidPattern))
+        #self.setupUi(self)
+
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowSystemMenuHint)
+
+        self.scene_canvas = QGraphicsScene()
+
         gradient = QRadialGradient(0,0, 600, 0,0)
         gradient.setColorAt(0, QColor("#00aaaa"))
         gradient.setColorAt(1, QColor("#eeeeee"))
-        self.gv_canvas.setBackgroundBrush(QBrush(gradient))
+        self.setBackgroundBrush(QBrush(gradient))
         self.history = CommandStack(MAX_HISTORY)
         self.icon_path = os.path.join(tempfile.gettempdir(), "icon.png")
-        self.canvas_path = os.path.join(tempfile.gettempdir(), "canvas.png")
-        self.tb_collection.setColumnCount(1)
-        self.load_collection(init=True)
         self.generate_icon()
 
 
@@ -43,12 +41,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.history.add_item(self.code)
 
     def update_ui(self):
-        self.make_label()
         self.pix = QPixmap(self.icon_path)
-        self.scene_icon.clear()
-        self.scene_icon.addPixmap(self.pix)
-        self.gv_icon.setScene(self.scene_icon)
-        self.statusbar.clearMessage()
+        self.scene_canvas.clear()
+        self.scene_canvas.addPixmap(self.pix)
         self.animation()
 
     def animation(self):
@@ -56,7 +51,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         OutElastic: Quick tiled;
         InOutElastic: centered then tiled;"""
 
-        mode = 10
+        mode = 12
         count = mode * mode
 
         scene = QGraphicsScene(-350, -350, 700, 700)
@@ -68,7 +63,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             items.append(item)
             scene.addItem(item.pixmap_item)
 
-        self.gv_canvas.setScene(scene)
+        self.setScene(scene)
 
         self.group = QParallelAnimationGroup()
         for i, item in enumerate(items):
@@ -76,23 +71,11 @@ class MainWindow(QMainWindow, Ui_MainWindow):
             anim.setStartValue(QPointF(-self.pix.width()/2, -self.pix.height()/2))
             anim.setEndValue(QPointF(((i%mode)-mode/2)*self.pix.width() + self.pix.width()/2,
                     ((i//mode)-mode/2)*self.pix.height() + self.pix.height()/2))
-            anim.setDuration(750+i*25)
-            #anim.setDuration(1500)
-            anim.setEasingCurve(QEasingCurve.InOutBack)
+            #anim.setDuration(550+i*25)
+            anim.setDuration(1800)
+            anim.setEasingCurve(QEasingCurve.InOutElastic)
             self.group.addAnimation(anim)
         self.group.start()
-
-    def make_label(self):
-        label_list = []
-        for item in self.history.item_list:
-            if self.code == item:
-                item = "<font color='blue'>%s</font>" % self.code
-            else:
-                item = str(item)
-            label_list.append(item)
-        count = MAX_HISTORY - len(label_list)
-        label_list.append("<br/>" * count)
-        self.lb_icon.setText("<br/>".join(label_list))
 
     def generate_icon_in_history_backward(self):
         self.history.move_cursor_backward()
@@ -120,33 +103,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.tb_collection.setCurrentCell(0,0)
         self.load_collection()
         self.generate_icon(code=old_code)
-
-    def parse_code(self, filename):
-        return filename.rpartition("-")[2].partition(".")[0]
-
-    def load_collection(self,init=False):
-
-        #if init:
-            #self.show()
-        self.thread = Load_Collection(self)
-        self.thread.partDone.connect(self.update_probar)
-        self.thread.procDone.connect(self.finish_probar)
-        self.thread.run()
-
-    def update_bar(self,val):
-        if self.probar.isHidden():
-            self.probar.show()
-        self.probar.setValue(val)
-
-    def finish_bar(self):
-        self.lb_probar.hide()
-        self.probar.hide()
-
-    def on_tb_collection_itemClicked(self):
-        self.generate_icon(
-                code=self.parse_code(str(self.tb_collection.currentItem().text())),
-                hist=True)
-
 
     def keyPressEvent(self, event):
         key = event.key()
@@ -178,62 +134,6 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 self.load_collection()
 
         QMainWindow.keyPressEvent(self, event)
-
-    def update_probar(self, val):
-        if self.probar.isHidden():
-            self.statusbar.showMessage("Loading collection...")
-            self.probar.show()
-        self.probar.setValue(val)
-
-    def finish_probar(self):
-        self.statusbar.clearMessage()
-        self.probar.hide()
-
-class Load_Collection(QThread):
-    partDone = pyqtSignal(int)
-    procDone = pyqtSignal(bool)
-
-    def __init__(self,main_window, parent=None):
-        super(Load_Collection, self).__init__(parent)
-        self.stopped = False
-        self.completed = False
-        self.main_window = main_window
-
-    def stop(self):
-        self.stopped = False
-
-    def run(self):
-        self.process()
-        self.stop()
-
-    def process(self):
-
-        collection_folder = "jpg"
-        if not os.path.exists(collection_folder):
-            os.mkdir(collection_folder)
-
-        self.main_window.setEnabled(False)
-        collection_path = os.path.join(os.getcwd(), collection_folder)
-        file_list = sorted(os.listdir(collection_path),
-                key=lambda f: os.path.getmtime(os.path.join(collection_path,f)),
-                reverse=True)
-
-        hash_list = []
-        count = len(file_list)
-        self.main_window.tb_collection.setRowCount(count)
-        self.main_window.tb_collection.clear()
-        for i,item in enumerate(file_list):
-            self.partDone.emit(i*100/count)
-
-            table_item = QTableWidgetItem(0)
-            hash_code = self.main_window.parse_code(item)
-            self.main_window.generate_icon(code=hash_code, ui=False)
-            table_item.setIcon(QIcon(QPixmap(self.main_window.icon_path)))
-            table_item.setText(item)
-            self.main_window.tb_collection.setItem(i,0,table_item)
-
-        self.procDone.emit(True)
-        self.main_window.setEnabled(True)
 
 class Pixmap(QObject):
 
